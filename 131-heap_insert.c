1d5e8a725cfbf9c41545fe3e680cf87564061367
ubonisrael <akpanudohubonisrael@gmail.com>
2024-02-02 04:09:45 +0100

    testing binary_tree_node

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -2,6 +2,27 @@
 void power_of_red(void);
 
 /**
+  * binary_tree_node - creates a binary tree node
+  * @parent: is a pointer to the parent node of the node to create
+  * @value: the value to be put in the new node
+  * Return: a pointer to the new node or NULL on failure
+  */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+
+	if (new_node == NULL)
+		return (NULL);
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	new_node->n = value;
+
+	return (new_node);
+}
+
+/**
  * heap_insert - inserts a value in Max Binary Heap
  * @root: double pointer to the root node of the Heap to insert the value
  * @value: the value to store in the node to be inserted
