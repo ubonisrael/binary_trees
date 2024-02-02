8cfc33097d76714fdd1bec704fd362d61c1be3c8
Drihmia <drihmia.redouane@gmail.com>
2024-02-02 04:00:11 +0100

    try_2

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -1,26 +1,7 @@
 #include "binary_trees.h"
 
 
-/**
-  * binary_tree_node - creates a binary tree node
-  * @parent: is a pointer to the parent node of the node to create
-  * @value: the value to be put in the new node
-  * Return: a pointer to the new node or NULL on failure
-  */
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
-{
-	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-
-	if (new_node == NULL)
-		return (NULL);
-	new_node->parent = parent;
-	new_node->left = NULL;
-	new_node->right = NULL;
-	new_node->n = value;
-
-	return (new_node);
-}
 /**
  * get_last_node - finds the last node in the last level of the heap
  * @root: double pointer to the root node of heap
